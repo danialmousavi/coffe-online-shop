ecfda69bc83f26953fa149fcb8044c7a37602098
danialmousavi <danialmoosavi69@gmail.com>
2025-09-21 12:04:10 +0330

    signup api

--- src/app/api/auth/route.js
+++
@@ -1,6 +0,0 @@
-import connectToDB from "../../../../configs/db";
-
-export async function GET(req) {
-    connectToDB();
-    return Response.json({message:"successfully"},{status:200})
-}
---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,42 @@
+import { generateAccessToken, hashPassword } from "@/utils/auth";
+import connectToDB from "@/configs/db";
+import userModel from "@/models/User";
+
+export async function POST(req) {
+  try {
+    connectToDB();
+    const body = await req.json();
+    const { name, email, password, phone } = body;
+
+    const isUserExist = await userModel.findOne({
+      $or: [{ name }, { email }, { phone }],
+    });
+    if (isUserExist) {
+      return Response.json(
+        { message: "The user already exist" },
+        { status: 422 }
+      );
+    }
+    const hashedPassword = await hashPassword(password);
+    const accessToken = generateAccessToken({ name, email, phone });
+    const users = await userModel.find({});
+
+    await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      role: users.length > 0 ? "USER" : "ADMIN",
+    });
+    return Response.json(
+      { message: "User created successfully" },
+      {
+        status: 201,
+        headers: { "Set-Cookie": `token=${accessToken};path=/;httpOnly=true` },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
