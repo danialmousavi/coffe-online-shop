d380ecc4904ff37e62cb1b846fb69a77706bdbc2
danialmousavi <danialmoosavi69@gmail.com>
2025-09-28 18:12:03 +0330

    route protection for user panel

--- src/components/layouts/UserPanelLayout.js
+++ src/components/layouts/UserPanelLayout.js
@@ -2,8 +2,15 @@ import React from "react";
 import styles from "./userPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-user/Sidebar";
 import Topbar from "@/components/modules/p-user/Topbar";
+import { userAuth } from "@/utils/userAuth";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }) => {
+const Layout = async ({ children }) => {
+  const user = await userAuth();
+  console.log("userrrrrr", user);
+  if(!user){
+    redirect("/login-register")
+  }
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
