69d5ca7c952921bdb5b632a7049952c2565aac28
danialmousavi <danialmoosavi69@gmail.com>
2025-10-02 20:25:41 +0330

    auth admins

--- src/components/layouts/AdminPanelLayout.js
+++ src/components/layouts/AdminPanelLayout.js
@@ -2,8 +2,18 @@ import React from "react";
 import styles from "./adminPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-admin/Sidebar";
 import Topbar from "@/components/modules/p-admin/Topbar";
+import { userAuth } from "@/utils/userAuth";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }) => {
+const Layout = async({ children }) => {
+  const user =await userAuth();
+  console.log("user in admin panel",user);
+  if(!user){
+   return redirect("/login-register")
+  }
+  if(user.role!="ADMIN"){
+   return redirect("/")
+  }
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
