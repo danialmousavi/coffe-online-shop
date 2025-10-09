9785c2066e2ed184f21b9b7de4b8b804f43af976
danialmousavi <danialmoosavi69@gmail.com>
2025-10-09 15:39:10 +0330

    show latest products

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,21 @@ import Articles from "@/components/templates/index/articles/Articles";
 import Banner from "@/components/templates/index/banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
 import Promote from "@/components/templates/index/promote/Promote";
+import connectToDB from "@/configs/db";
+import productModel from "@/models/Product";
 import { userAuth } from "@/utils/userAuth";
 
 export default async function Home() {
   const user=await userAuth()
+  connectToDB();
+  const products=await productModel.find({},"-comments").sort({_id:-1}).limit(8).lean();
+  console.log("productssssssss",products);
   
   return (
     <>
       <Navbar user={JSON.parse(JSON.stringify(user))} />
       <Banner />
-      <Latest />
+      <Latest products={JSON.parse(JSON.stringify(products))}/>
       <Promote />
       <Articles />
       <Footer />
--- src/components/modules/product/Product.js
+++ src/components/modules/product/Product.js
@@ -3,13 +3,14 @@ import styles from "./product.module.css";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { CiSearch, CiHeart } from "react-icons/ci";
 
-const Card = ({name,price}) => {
+const Card = ({name,price,img,score}) => {
   return (
     <div className={styles.card}>
       <div className={styles.details_container}>
         <img
-          src="https://set-coffee.com/wp-content/uploads/2021/10/041-430x430.png"
+          src={img?img:"https://set-coffee.com/wp-content/uploads/2021/10/041-430x430.png"}
           alt=""
+          
         />
         <div className={styles.icons}>
           <Link href="/">
@@ -30,11 +31,13 @@ const Card = ({name,price}) => {
           {name}
         </Link>
         <div>
-          <FaStar />
-          <FaStar />
-          <FaStar />
-          <FaRegStar />
-          <FaRegStar />
+          {new Array(score).fill(0).map((item, index) => (
+            <FaStar key={index} />
+          ))}
+
+          {new Array(5 -score).fill(0).map((item, index) => (
+            <FaRegStar key={index} />
+          ))}
         </div>
         <span>{price?.toLocaleString()} تومان</span>
       </div>
